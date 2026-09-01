745151fb213f661dad1ce247a18f58bd0b4812f3
Arjuna Notanubun <arjuna_notanubun_33rpl@student.smktelkom-mlg.sch.id>
2026-09-01 19:07:25 +0700

    Sesi 5: tambah dokumentasi Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Perpustakaan XII R2 API')
+    .setDescription('Dokumentasi API sistem perpustakaan sekolah')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
